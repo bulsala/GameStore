170adc94ff368a6706398cd8024d1c5d0572e738
bulsala <bulsala@yandex.ru>
2016-07-26 23:49:24 +0300

    [#br_002] Создал контроллер навигации + генерация списка категорий

---
+++ GameStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GameStore.Domain.Abstract;
+
+namespace GameStore.WebUI.Controllers
+{
+    using System.Web.Mvc;
+
+    public class NavController : Controller
+    {
+        public IGameRepository repository;
+
+        public NavController(IGameRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Games
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
+        }
+    }
+}
